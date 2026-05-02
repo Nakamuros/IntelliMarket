30eee424b0916c4e0b927c87997058e1bf62fa8b
Angel Diaz <agabrieldchavez@gmail.com>
2026-05-01 19:28:33 -0500

    feat(auth): Se realizo unos diminutos ajustes a los codigos en model/User

--- src/main/java/com/intellimarket/api/auth/dto/RegisterRequest.java
+++ src/main/java/com/intellimarket/api/auth/dto/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.intellimarket.api.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,17 +14,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RegisterRequest {
 
-    @NotBlank(message = "Email is required")
+    @Size(max = 30) @NotBlank (message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
 
     @NotBlank(message = "Password is required")
     private String password;
 
-    @NotBlank(message = "First name is required")
+    @Size(max = 30) @NotBlank(message = "First name is required")
     private String firstName;
 
-    @NotBlank(message = "Last name is required")
+    @Size(max = 30) @NotBlank(message = "Last name is required")
     private String lastName;
 }
 
