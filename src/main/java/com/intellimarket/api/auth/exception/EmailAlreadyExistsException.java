3674587edcb092def2ff7eeea41fe83d470f0963
JUANRV27 <rodriguezvelasquezjuanjose11@gmail.com>
2026-05-08 22:47:04 -0500

    Feat: nucleo del módulo de Order

--- src/main/java/com/intellimarket/api/auth/exception/EmailAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.intellimarket.api.auth.exception;
-
-import com.intellimarket.api.shared.exception.BusinessRuleException;
-
-public class EmailAlreadyExistsException extends BusinessRuleException {
-    public EmailAlreadyExistsException(String email) {
-        super("El email " + email + " ya está registrado en el sistema.");
-    }
-}
--- src/main/java/com/intellimarket/api/auth/exception/InvalidCredentialsException.java
+++
@@ -1,9 +0,0 @@
-package com.intellimarket.api.auth.exception;
-
-import com.intellimarket.api.shared.exception.BusinessRuleException;
-
-public class InvalidCredentialsException extends BusinessRuleException {
-    public InvalidCredentialsException() {
-        super("Correo o contraseña incorrectos.");
-    }
-}
