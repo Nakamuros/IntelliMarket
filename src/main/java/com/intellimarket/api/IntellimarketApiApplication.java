a691c2f480c73eb471a276daadea1d7c97de77ee
JUANRV27 <rodriguezvelasquezjuanjose11@gmail.com>
2026-05-12 13:48:36 -0500

    Comentario

--- src/main/java/com/intellimarket/api/IntellimarketApiApplication.java
+++ src/main/java/com/intellimarket/api/IntellimarketApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//funcion principal de la aplicación
 public class IntellimarketApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(IntellimarketApiApplication.class, args);
     }
