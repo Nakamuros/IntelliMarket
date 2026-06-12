0d961b2755ff3111953d4998bbee21319c77e22c
MauColab <mauriciocanchisf@gmail.com>
2026-06-12 00:55:53 -0500

    feat: Buenas nuevas: Corrección en una pequeña línea de SecurityConfig, y ALELUYA.

--- src/main/java/com/intellimarket/api/inventory/controller/InventoryController.java
+++ src/main/java/com/intellimarket/api/inventory/controller/InventoryController.java
@@ -19,6 +19,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/inventory")
+
 //@RequestMapping("/api/inventory/stores/{storeId}")
 @RequiredArgsConstructor
 //@CrossOrigin(origins = "*")
