cd0ac19af85ef2a309925e971249be2487f4dd09
Nakamuros <agabrieldchavez@gmail.com>
2026-06-15 14:52:20 -0500

    fix: se corrigieron las rutas del controlador

--- src/main/java/com/intellimarket/api/inventory/controller/InventoryController.java
+++ src/main/java/com/intellimarket/api/inventory/controller/InventoryController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/inventory")
+@RequestMapping("/api/v1/inventory")
 
 //@RequestMapping("/api/inventory/stores/{storeId}")
 @RequiredArgsConstructor
