1effe8d27a4655076fd7d5e719d1b8189a35e181
JUANRV27 <rodriguezvelasquezjuanjose11@gmail.com>
2026-06-15 22:34:28 -0500

    configurando render.yaml - correcciones3

--- src/main/java/com/intellimarket/api/config/SecurityConfig.java
+++ src/main/java/com/intellimarket/api/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
